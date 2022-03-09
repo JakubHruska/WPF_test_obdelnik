e0bcf8ea96f0065b07de6900f3c97ed6cbbe5032
Jakub Hruška <u19.410@pslib.cloud>
2022-03-09 09:58:21 +0100

    Add project files.

---
+++ WPF_test_obdelnik/ViewModel/ViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_test_obdelnik.ViewModel
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        private double _width;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //Použití jinde - hotovo
+        public void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public double WidthValue
+        {
+            get { return _width; }
+            set { _width = value; NotifyPropertyChanged(); }
+        }
+    }
+}
